3f8069b7f3e1ecf28ce546a79104d1a2d7adbdc1
Quang Dung <truongquangdung0609@gmail.com>
2024-06-12 02:17:55 +0200

    feat: update mechanic to change card

--- CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardSelectionVC.swift
@@ -10,9 +10,10 @@ import UIKit
 class CardSelectionVC: UIViewController {
 
     @IBOutlet var cardImageView: UIImageView!
-    
     @IBOutlet var buttons: [UIButton]!
+    var cards: [UIImage] = Card.allValues
     
+    var timer: Timer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,16 +21,32 @@ class CardSelectionVC: UIViewController {
         for button in buttons{
             button.layer.cornerRadius = 8
         }
+        startTimer()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer.invalidate()
+    }
+    
+    func startTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    
+    @objc func showRandomImage(){
+        cardImageView.image = cards.randomElement() ?? UIImage(named:"AS")
         
     }
     
 
     @IBAction func stopButtonTapped(_ sender: UIButton) {
-            }
+        timer.invalidate()
+    }
     
     @IBAction func restartButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        startTimer()
     }
-    
-    @IBAction func rulesButtonTapped(_ sender: UIButton) {
-    }
+
 }
